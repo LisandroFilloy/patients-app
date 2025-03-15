ee8f6e1d0b78ef585e14886629596b7ab55abf54
lisandrofilloy <lisandrofilloy@gmail.com>
2025-03-15 00:38:23 -0300

    patient cards + add patient

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,67 +1,118 @@
+"use client"
 
 import Image from "next/image";
+import { useState } from "react";
 
-const TEST_PATIENTS = [
+let TEST_PATIENTS = [
   {
+    'id': 1,
     'full_name': 'Tomas Lopez',
     'email_adress': 'tomas.lopez@gmail.com',
     'phone_number': '+5491133334444',
-    'id_photo': 'https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png'
+    'id_photo': 'DNI-prueba.jpg'
   },
   {
-    'full_name': 'Juan Perez',
-    'email_adress': 'juan.perez@gmail.com',
+    'id': 2,
+    'full_name': 'Tomas Lopez',
+    'email_adress': 'tomas.lopez@gmail.com',
     'phone_number': '+5491133334444',
-    'id_photo': 'https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png'
+    'id_photo': 'DNI-prueba.jpg'
   },
   {
-    'full_name': 'Maria Gomez',
-    'email_adress': 'maria.gomez@gmail.com',
+    'id': 3,
+    'full_name': 'Tomas Lopez',
+    'email_adress': 'tomas.lopez@gmail.com',
     'phone_number': '+5491133334444',
-    'id_photo': 'https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png'
+    'id_photo': 'DNI-prueba.jpg'
   },
   {
-    'full_name': 'Pedro Rodriguez',
-    'email_adress': 'pedro.rodriguez@gmail.com',
+    'id': 4,
+    'full_name': 'Tomas Lopez',
+    'email_adress': 'tomas.lopez@gmail.com',
     'phone_number': '+5491133334444',
-    'id_photo': 'https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png'
-  }
+    'id_photo': 'DNI-prueba.jpg'
+  },
+  {
+    'id': 5,
+    'full_name': 'Tomas Lopez',
+    'email_adress': 'tomas.lopez@gmail.com',
+    'phone_number': '+5491133334444',
+    'id_photo': 'DNI-prueba.jpg'
+  },
+  {
+    'id': 6,
+    'full_name': 'Tomas Lopez',
+    'email_adress': 'tomas.lopez@gmail.com',
+    'phone_number': '+5491133334444',
+    'id_photo': 'DNI-prueba.jpg'
+  },
+  {
+    'id': 7,
+    'full_name': 'Tomas Lopez',
+    'email_adress': 'tomas.lopez@gmail.com',
+    'phone_number': '+5491133334444',
+    'id_photo': 'DNI-prueba.jpg'
+  },
+  {
+    'id': 8,
+    'full_name': 'Tomas Lopez',
+    'email_adress': 'tomas.lopez@gmail.com',
+    'phone_number': '+5491133334444',
+    'id_photo': 'DNI-prueba.jpg'
+  },
+  {
+    'id': 9,
+    'full_name': 'Tomas Lopez',
+    'email_adress': 'tomas.lopez@gmail.com',
+    'phone_number': '+5491133334444',
+    'id_photo': 'DNI-prueba.jpg'
+  },
+  {
+    'id': 10,
+    'full_name': 'Tomas Lopez',
+    'email_adress': 'tomas.lopez@gmail.com',
+    'phone_number': '+5491133334444',
+    'id_photo': 'DNI-prueba.jpg'
+  },
 ];
 
 export default function Home() {
+  const [expanded, setExpanded] = useState<null | number>(null);
 
   return (
     <div className="m-8">
       <h1 className="text-4xl flex justify-center mb-8">Patients</h1>
-      <div className="overflow-hidden rounded-lg">
-        <table className="border-2 border-gray-200 mx-auto w-full">
-          <thead>
-            <tr className="bg-gray-200">
-              <th className="p-2 text-center">Full Name</th>
-              <th className="p-2 text-center">Email Address</th>
-              <th className="p-2 text-center">Phone Number</th>
-              <th className="p-2 text-center">ID Photo</th>
-            </tr>
-          </thead>
-          <tbody>
-            {TEST_PATIENTS.map((patient) => {
-              return (
-                <tr className="hover:bg-gray-100" key={patient.email_adress}>
-                  <td className="p-2 text-center">{patient.full_name}</td>
-                  <td className="p-2 text-center">{patient.email_adress}</td>
-                  <td className="p-2 text-center">{patient.phone_number}</td>
-                  <td className="p-2">
-                    <img
-                      src={patient.id_photo}
-                      alt="ID"
-                      className="rounded-lg w-16 h-16 object-cover"
-                    />
-                  </td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
+      <div className="flex flex-col gap-4 mx-[30%] min-w-96">
+        <div className="hover:bg-green-100 border-1 border-gray-200 rounded-md shadow-lg flex items-center p-4 flex justify-center items-center cursor-pointer">
+          <span className="text-8xl text-green-500">+</span>
+        </div>
+        {TEST_PATIENTS.map((patient) => {
+          return (
+            <div onClick={() => setExpanded(expanded === patient.id ? null : patient.id)}
+              className={`hover:scale-102 border-1 border-gray-200 rounded-md shadow-lg flex flex-col gap-4 items-center p-4 cursor-pointer"
+                }`}
+              key={patient.email_adress}>
+
+              <div className="text-center text-xl">{patient.full_name}</div>
+
+              <div>
+                <img
+                  src={patient.id_photo}
+                  alt="ID"
+                  className="rounded-lg w-[214px] h-[135px] object-cover"
+                />
+              </div>
+
+              {expanded === patient.id && (
+                <div className="mt-2 text-start">
+                  <p><span className="font-bold">email:</span> {patient.email_adress}</p>
+                  <p><span className="font-bold">Phone:</span> {patient.phone_number}</p>
+                </div>
+              )}
+            </div>
+
+          );
+        })}
       </div>
     </div>
   );
