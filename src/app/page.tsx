5e343b070acc9cabbdaab7a1892eba688b60b01d
lisandrofilloy <lisandrofilloy@gmail.com>
2025-03-15 01:00:51 -0300

    WIP form modal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -78,12 +78,15 @@ let TEST_PATIENTS = [
 
 export default function Home() {
   const [expanded, setExpanded] = useState<null | number>(null);
+  const [newPatientModal, setNewPatientModal] = useState(false);
 
   return (
     <div className="m-8">
       <h1 className="text-4xl flex justify-center mb-8">Patients</h1>
       <div className="flex flex-col gap-4 mx-[30%] min-w-96">
-        <div className="hover:bg-green-100 border-1 border-gray-200 rounded-md shadow-lg flex items-center p-4 flex justify-center items-center cursor-pointer">
+        <div
+          onClick={() => { setNewPatientModal(true) }}
+          className="hover:bg-green-100 border-1 border-gray-200 rounded-md shadow-lg flex items-center p-4 flex justify-center items-center cursor-pointer">
           <span className="text-8xl text-green-500">+</span>
         </div>
         {TEST_PATIENTS.map((patient) => {
@@ -114,6 +117,29 @@ export default function Home() {
           );
         })}
       </div>
+      {newPatientModal && (
+        <div className="w-screen h-screen z-100 fixed top-0 left-0 flex justify-center items-center">
+          <div className="absolute inset-0 bg-black opacity-80" onClick={() => setNewPatientModal(false)}></div>
+          <div className="bg-white w-[500px] h-[600px] z-10 relative border-black rounded-lg flex flex-col gap-4 p-8 items-center text-xl">
+            <div>
+              <h1 className="px-1">Full Name:</h1>
+              <input className="border-2 rounded-sm p-1 w-[300px]"></input>
+            </div>
+            <div>
+              <h1 className="px-1">Email Address:</h1>
+              <input className="border-2 rounded-sm p-1 w-[300px]"></input>
+            </div>
+            <div>
+              <h1 className="px-1">Phone Number:</h1>
+              <input className="border-2 rounded-sm p-1 w-[300px]"></input>
+            </div>
+            <div>
+              <h1 className="px-1 mt-4">Drag and drop Document</h1>
+            </div>
+            {/* Modal Content */}
+          </div>
+        </div>
+      )}
     </div>
   );
 }
